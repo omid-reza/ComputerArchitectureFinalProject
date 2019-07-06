b55403b1ad6c9c532a9c7a8907d655b900c779c2
Omid Reza <snip77.orh@gmail.com>
2019-07-06 14:23:50 +0430

    add ability to load inst memory from binary.txt

--- toy/Alu.v
+++ toy/Alu.v
@@ -12,24 +12,3 @@ module Alu (input [2:0]alu_op,input [15:0]a,src , output reg[15:0]rd, output reg
 	 	assign z = (rd==16'b0 && c==1'b0)?1'b1:1'b0;
 	end
 endmodule
-
-module AluTb;
-	reg [15:0]a,b;
-	reg [2:0]alu_operion;
-	wire [15:0]reponse;
-	wire zero;
-	reg clk;
-	wire carry;
-	Alu alu_ins(alu_operion, a, b, reponse, zero, carry);
-	initial begin  
-        clk = 0;    
-        forever #50 clk = ~clk;  
-    end
-    initial begin
-    	#10 a=16'b0; b=16'b0; alu_operion=3'b0;
-    	#10 a=16'b0000000000000001;b=16'b0000000000000001;
-    	#200 a=16'b1111111111111111;alu_operion=3'b100;
-    	#200 a=16'b0000000000000001;
-    	#200 a=16'b0;b=16'b0;alu_operion=3'b0;
-    end
-endmodule
--- toy/Memory.v
+++ toy/Memory.v
@@ -1,13 +1,13 @@
 module InMemory (input [11:0]pc, output [15:0]Inst);
-	integer i;
 	reg [15:0]ram[4095:0];
-	initial begin 
-	  	ram[0]=16'b0000000000001000;
-	  	ram[8]=16'b1110000000000000;
-	  	ram[9]=16'b0001000000000001;
-		ram[10]=16'b0001000000000001;
-		ram[11]=16'b0001000000000001;
-		ram[12]=16'b0001000000000011;
+	integer file_id;
+	integer i=0;
+	initial begin
+		file_id=$fopen("..\\app\\binary.txt","rb");
+		while (! $feof(file_id)) begin
+			$fscanf(file_id,"%b\n",ram[i]);
+			i=i+1;
+		end
 	end
  	assign Inst = ram[pc];  
 endmodule
@@ -18,7 +18,7 @@ module DataMemory(input clk,rd,wr,input [11:0]mem_access_addr,input[15:0]wd , ou
     initial begin  
 		for(i=0;i<4096;i=i+1)  
 			ram[i] <= 16'd0;  
-    end  
+    end
     initial begin 
       ram[0] <= 16'b0000000000000010;
       ram[1] <= 16'b0000000000001000;
