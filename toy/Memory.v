8e5cbe29fdb887ac9447cce8902e0df599c3562b
Omid Reza <snip77.orh@gmail.com>
2019-07-06 14:56:28 +0430

    load data mem from data.txt

--- toy/Memory.v
+++ toy/Memory.v
@@ -13,17 +13,22 @@ module InMemory (input [11:0]pc, output [15:0]Inst);
 endmodule
 
 module DataMemory(input clk,rd,wr,input [11:0]mem_access_addr,input[15:0]wd , output  [15:0]Rd);
-	integer i;  
+	integer address;
+	integer file_id;
+	integer i;
+	reg [27:0]line;
 	reg [15:0] ram [4095:0];  
     initial begin  
 		for(i=0;i<4096;i=i+1)  
 			ram[i] <= 16'd0;  
     end
-    initial begin 
-      ram[0] <= 16'b0000000000000010;
-      ram[1] <= 16'b0000000000001000;
-      ram[2] <= 16'b0000000001000000;
-      ram[3] <= 16'b0000000000010001;
+    initial begin
+    	file_id=$fopen("..\\app\\data.txt","rb");
+		while (! $feof(file_id)) begin
+			$fscanf(file_id,"%b\n",line);
+			address=line[27:16];
+			ram[address]<=line[15:0];
+		end
   end
   always@(posedge clk)
   begin
