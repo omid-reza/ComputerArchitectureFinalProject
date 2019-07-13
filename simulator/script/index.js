0cc96436ed2d50718df4208ac3e8e2d8d8a89949
Omid Reza <snip77.orh@gmail.com>
2019-07-13 09:56:24 +0430

    ChangeLog: 1. change compiler folder from app to compiler 2. add simulator files based on web

---
+++ simulator/script/index.js
@@ -0,0 +1,23 @@
+new Vue({
+  el: '#app',
+  data: {
+    files:null,
+    compile_type:null,
+    errors:[]
+  },
+  methods:{
+    checkForm: function (e) {
+      this.errors = [];
+      if (!this.files)
+        this.errors.push('File requiere');
+      if (!this.compile_type)
+        this.errors.push('Compile Type Require');
+      if (!this.errors.length)
+        return true;
+      e.preventDefault();
+    },
+    previewFiles() {
+			this.files = this.$refs.myFiles.files
+	  }
+  }
+})
